7188d0cf55b2ce8fa124c57849e364175bb79646
Ties Noordhuis <tiesnoordhuis@gmail.com>
2017-03-06 17:51:16 +0100

    ini

---
+++ tiesjs.js
@@ -0,0 +1,7 @@
+// chrome extention js
+
+var b = document.getElementsByClassName("aos T-I-J3 J-J5-Ji");
+b[0].style.background = "#f5f5f5 no-repeat right top";
+b[0].innerHTML = "settings";
+b[0].style.height = "25px";
+b[0].style.width = "40px";
